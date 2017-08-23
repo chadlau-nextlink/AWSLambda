b0c5ad4886e18af21e9f5f4f7a4a95f6c6360dae
chadlau-nextlink <31281550+chadlau-nextlink@users.noreply.github.com>
2017-08-23 22:00:11 +0800

    Update stop-ec2rds.py

--- stop-ec2rds.py
+++ stop-ec2rds.py
@@ -1,12 +1,12 @@
 import boto3
 region = 'us-east-1'
-instances = ['i-0ef96d05207ab375c']
-databases = ['test-chad']
+instances = ['instanceid']
+databases = ['dbidentifier']
 
 def lambda_handler(event, context):
 	ec2 = boto3.client('ec2', region_name=region)
 	rds = boto3.client('rds')
 	ec2.stop_instances(InstanceIds=instances)
-	rds.stop_db_instance(DBInstanceIdentifier='test-chad')
+	rds.stop_db_instance(DBInstanceIdentifier='dbidentifier')
 	print 'stopped your instances: ' + str(instances)
 	print 'stopped your instances: ' + str(databases)
